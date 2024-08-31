716f062ce95a87bbcc014e7e313d7ea82d1db94f
Patel Vaishnavi <patelvaishnavim@gmail.com>
2024-09-01 03:29:27 +0530

    Test Case Failed Again : IllegalArgumentException not throwing exception

--- src/main/java/org/vaishnavii/Book.java
+++ src/main/java/org/vaishnavii/Book.java
@@ -1,5 +1,22 @@
 package org.vaishnavii;
 
+import java.time.Year;
+
 public class Book{
+    private String isbn;
+    private String title;
+    private String author;
+    private Year publicationYear;
+
+    public Book(String isbn, String title, String author, Year publicationYear) {
+
+
+
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
 
 }
