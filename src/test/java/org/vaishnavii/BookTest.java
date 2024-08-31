b0eda4ffdb4b5b7927f338b5e7b1cff4138d3798
Patel Vaishnavi <patelvaishnavim@gmail.com>
2024-09-01 03:12:55 +0530

    First Failed Test Case : Book Constructor not found

--- src/test/java/org/vaishnavii/BookTest.java
+++ src/test/java/org/vaishnavii/BookTest.java
@@ -1,5 +1,15 @@
 package org.vaishnavii;
 
-public class BookTest {
+import org.junit.jupiter.api.Test;
 
+import java.time.Year;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookTest {
+    @Test
+    public void throwExceptionWhenISBNisNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Book(null, "Clean Code", "Robert Cecil Martin", Year.of(2012)));
+        assertEquals("ISBN should not be null or empty", exception.getMessage());
+    }
 }
