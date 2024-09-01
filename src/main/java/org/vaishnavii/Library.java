e5809b222027ffbe41ae88e10c4730a58370429c
Patel Vaishnavi <patelvaishnavim@gmail.com>
2024-09-01 13:44:36 +0530

    Test Case Passed : Library Name Validation Added

--- src/main/java/org/vaishnavii/Library.java
+++ src/main/java/org/vaishnavii/Library.java
@@ -5,6 +5,10 @@ public class Library {
     String name;
 
     public Library(String name) {
+
+        if(name == null) {
+            throw new IllegalArgumentException("Library Name Should not be null");
+        }
         this.name = name;
     }
 }
