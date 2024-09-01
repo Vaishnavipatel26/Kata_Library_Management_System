17d781fb56cd82969288c01092bad096c7501302
Patel Vaishnavi <patelvaishnavim@gmail.com>
2024-09-01 13:27:13 +0530

    Library Class Not Found

--- src/test/java/org/vaishnavii/LibraryTest.java
+++ src/test/java/org/vaishnavii/LibraryTest.java
@@ -1,6 +1,15 @@
 package org.vaishnavii;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class LibraryTest {
-    
+
+    @Test
+    public void testFailWithoutProperConstructor() {
+        Library library = new Library("Vaishnavi");
+        assertNotNull(library);
+    }
 
 }
