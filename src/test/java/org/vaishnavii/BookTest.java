247152876b2e96dd9a929f8478d9f7f76fff57b2
Patel Vaishnavi <patelvaishnavim@gmail.com>
2024-09-01 12:13:22 +0530

    Test case added successfully : Throw exception when Author is Empty

--- src/test/java/org/vaishnavii/BookTest.java
+++ src/test/java/org/vaishnavii/BookTest.java
@@ -18,4 +18,10 @@ public class BookTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Book("9780132350884", null, "Robert Cecil Martin", Year.of(2012)));
         assertEquals("title should not be null or empty", exception.getMessage());
     }
+
+    @Test
+    public void throwExceptionWhenAuthorIsEmpty() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Book("9780132350884", "Clean Code", "", Year.of(2012)));
+        assertEquals("author should not be null or empty", exception.getMessage());
+    }
 }
